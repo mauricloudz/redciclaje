63b1c984194ebc25ef51241646a00af33b91cbee
Mauricio Poblete <79657289+mauricloudz@users.noreply.github.com>
2023-11-20 02:07:57 -0300

    updated

--- script.js
+++ script.js
@@ -1,30 +1,24 @@
-// Variable global para el objeto de mapa
 var mapa;
 
-// Función para inicializar el mapa
 function inicializarMapa() {
     if (typeof google === 'undefined' || typeof google.maps === 'undefined') {
         console.error('El objeto "google" o "google.maps" no está definido. Verifica la carga del script de Google Maps.');
         return;
     }
 
-    // Coordenadas de ejemplo (ajusta según sea necesario)
     var coordenadas = { lat: 40.748817, lng: -73.985428 };
 
-    // Crea un nuevo mapa en el contenedor con ID "google-map"
     mapa = new google.maps.Map(document.getElementById('google-map'), {
         zoom: 12,
         center: coordenadas
     });
 
-    // Crea un marcador en el mapa
     var marcador = new google.maps.Marker({
         position: coordenadas,
         map: mapa,
         title: 'Ubicación Actual'
     });
 
-    // Ajusta el tamaño del mapa cuando cambia el tamaño de la ventana
     window.addEventListener('resize', function () {
         var center = mapa.getCenter();
         google.maps.event.trigger(mapa, 'resize');
@@ -34,53 +28,50 @@ function inicializarMapa() {
 
 document.addEventListener("DOMContentLoaded", function () {
     var menuBtn = document.getElementById("navbar-toggler");
-    var menuNav = document.getElementById("navbarNav");
+    var navbarNav = document.querySelector(".navbar-nav");
+
+    if (!menuBtn || !navbarNav) {
+        console.error("No se encontraron elementos del menú.");
+        return;
+    }
 
     menuBtn.addEventListener("click", function () {
-        toggleMenu();
+        navbarNav.classList.toggle("abierto");
     });
 
-    var app = document.getElementById("app");
-    app.addEventListener("click", function (event) {
-        if (event.target !== menuBtn && !menuNav.contains(event.target)) {
-            closeMenu();
-        }
+    var opcionesMenu = document.querySelectorAll(".navbar-nav ul li a");
+    opcionesMenu.forEach(function(opcion) {
+        opcion.addEventListener("click", function(event) {
+            event.preventDefault();
+            var pestana = this.getAttribute("href").substring(1);
+            cambiarPestana(pestana);
+            navbarNav.classList.remove("abierto");
+        });
     });
 
-    // Llama a la función inicializarMapa después de la carga del DOM
     inicializarMapa();
 });
 
-function toggleMenu() {
-    var menuNav = document.getElementById("navbarNav");
-    menuNav.style.width = (menuNav.style.width === "250px") ? "0" : "250px";
-}
-
-function closeMenu() {
-    var menuNav = document.getElementById("navbarNav");
-    menuNav.style.width = "0";
-}
-
 function cambiarPestana(pestana) {
-    var pestañas = document.getElementsByClassName("pestaña");
-    for (var i = 0; i < pestañas.length; i++) {
-        pestañas[i].classList.remove("mostrar");
+    console.log("Cambiando a la pestaña:", pestana);
+    var pestanaActual = document.getElementById(pestana);
+
+    if (pestanaActual) {
+        var pestañas = document.getElementsByClassName("pestaña");
+        for (var i = 0; i < pestañas.length; i++) {
+            pestañas[i].classList.remove("mostrar");
+        }
+
+        pestanaActual.classList.add("mostrar");
+
+        if (pestana == 'mapa') {
+            setTimeout(function () {
+                google.maps.event.trigger(mapa, 'resize');
+                var nuevasCoordenadas = { lat: 40.748817, lng: -73.985428 };
+                mapa.setCenter(nuevasCoordenadas);
+            }, 500);
+        }
+    } else {
+        console.error("Elemento con ID " + pestana + " no encontrado.");
     }
-
-    document.getElementById(pestana).classList.add("mostrar");
-
-    // Si la pestaña seleccionada es 'mapa', forzamos la actualización del mapa
-    if (pestana === 'mapa') {
-        setTimeout(function () {
-            google.maps.event.trigger(mapa, 'resize');
-
-            // Ajusta el centro del mapa a tus coordenadas deseadas
-            var nuevasCoordenadas = { lat: 40.748817, lng: -73.985428 };
-            mapa.setCenter(nuevasCoordenadas);
-        }, 500);
-    }
-
-    // Cerrar el menú después de cambiar de pestaña (opcional)
-    closeMenu();
 }
-
